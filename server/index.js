17855cc937386affca5cc395df65c68e0b016ac4
Minmgf <manuelinm2015@gmail.com>
2024-11-25 18:38:31 -0500

    1

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("");
+mongoose.connect("mongodb+srv://usco:usco@cluster0.9ao6n.mongodb.net/users");
 
 // LOGIIIIIIIIIIIIIN]
 app.post("/login", (req, res) => {
